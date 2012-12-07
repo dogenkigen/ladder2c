6cb8d69400e06bfe6cd00c7b81539c31d4bc503a
mlaskows <maciej.laskowski@alcatel-lucent.com>
2012-12-07 12:42:59 +0100

    Gui improvement and little code cleanup

--- index.py
+++ index.py
@@ -4,15 +4,11 @@ import os
 
 app = Flask(__name__)
 
-@app.route("/", methods=['POST','GET'])
+@app.route("/", methods=['POST', 'GET'])
 def index():
 	return render_template('/index.html')
 
-@app.route("/3", methods=['POST','GET'])
-def index2():
-	return render_template('/index3.html')
-
-@app.route("/add", methods=['POST','GET'])
+@app.route("/add", methods=['POST', 'GET'])
 def add_entry():
 #	f = app.open_resource('static/mylist.json')
 #	return json.dumps(f.read())
@@ -20,11 +16,14 @@ def add_entry():
 	
 	return jsonify(json.load(d))
 
-@app.route("/add2", methods=['POST','GET'])
+@app.route("/add2", methods=['POST', 'GET'])
 def add_entry2():
 	var1 = request.form["var1"]
 	var2 = request.form["var2"]
-	return jsonify(obj1={'propertyA':var1,'propertyB':var2})
+	return jsonify(obj1={'propertyA':var1, 'propertyB':var2})
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    #To make this publicly available simply set parameter host='0.0.0.0'.
+	app.run(debug=True, host='0.0.0.0')
+
+
