ed9a18b58da4d7a2fa15fc7e7cc6a7c8be4b4f6a
mlaskows <maciej.laskowski@alcatel-lucent.com>
2012-12-13 17:40:08 +0100

    Yet not generator

--- src_index/invoke.py
+++ src_index/invoke.py
@@ -2,16 +2,106 @@
 
 from lib.CodeGenerator import *
 from lib.Condition import *
+from lxml import etree
+from xml.etree.ElementTree import tostring
+from debian.debtags import output
+import xml.etree.ElementTree as ET
 
 
+#"""
 
+def recurse(object, super=None):
+    if object.tag != "elem":
+        return recurse(object.find(".//*"), object.tag)
+    else:
+        if super == "not":
+            print object.attrib["id"]
+            return Condition.logicNot(config.get("elements", object.attrib["id"]))
+        
+        elif super == "and":
+            elemsList = []
+            for elem in object:
+                elemsList.append(object.attrib["id"])
+                
+            return Condition.logicNot(config.get("elements", elemsList))
+        
+        elif super == "or":
+            elemsList = []
+            for elem in object:
+                elemsList.append(object.attrib["id"])
+        
+            return Condition.logicNot(config.get("elements", elemsList))
+                                      
+        
+                
+            
+        #"""
+            
+#"""    
+           
+
+#load files
+fxml = open('program.xml')
+fxsd = open('schema.xsd')
+
+#create parser which will be validate xsd schema
+parser = etree.XMLParser(dtd_validation=True)
+
+#convert files to strings
+xsdStr = ""
+for line in fxsd:
+    xsdStr = xsdStr + line
+    
+xmlStr = ""
+for line in fxml:
+    xmlStr = xmlStr + line
+
+schema_root = etree.XML(xsdStr)
+schema = etree.XMLSchema(schema_root)
+#load schema to parser
+parser = etree.XMLParser(schema=schema)
+
+#validate xml
+root = etree.fromstring(xmlStr, parser)
 config = ConfigParser.ConfigParser()
 config.readfp(open("conf/stm32f103vct6.conf"))
 
-gen = CodeGenerator(config)
+
+
+countersList = []
+
+elements = root.getiterator("elements")
+
+for element in elements:
+    #print tostring(element)
+    for counter in element.getiterator("counter"):
+        #print tostring(counter)
+        countersList.append([counter.attrib["id"], counter.attrib["countTo"], counter.attrib["target"]])
+
+
+gen = CodeGenerator(config, countersList)
+
+
+
+for output in root.find("diagram").findall("output"):
+    gen.appendCondtion(recurse(output), config.get("elements", output.attrib["id"]))
+        
+
+
+"""
 
 gen.appendCondtion(Condition.logicAnd(config.get("elements", "y1"), config.get("elements", "y2")), config.get("elements", "y1_type_set"))
 gen.appendCondtion(Condition.logicNot(config.get("elements", "y1")), config.get("elements", "y1_type_set"))
 
+gen.appendCondtion(Condition.logicNot(config.get("elements", "y1")), "C1")
 
+
+
+
+"""
 print gen.getCode()
+
+
+"""
+            for counter in elements.getiterator("counter"):
+                if output == counter.attrib["id"]:"""
--- src_index/lib/CodeGenerator.py
+++ src_index/lib/CodeGenerator.py
@@ -21,25 +21,47 @@ from Condition import *
 class CodeGenerator:
     """Generates all C code."""
 
-    def __init__(self, config):
+    def __init__(self, config, counters):
         self.config = config
         self.code = ""
+        self.counters = counters
         self.__begin()
     
     def __begin(self):
         """Initialize code. Add includes etc."""
         
-        self.code = self.config.get("base", "header")
+        #Add includes
+        self.code = self.config.get("base", "header") + "\n"
         for include in string.split(self.config.get("base", "includes"), " "):
-            self.code = self.code + "#include \"" + include + "\"\n"
-        self.code = self.code + "int main(void){while(1){"
+            self.code = self.code + "#include <" + include + ">\n"
+        
+        #Add functions
+        self.code = self.code + "\n"
+        for function in self.config.get("base", "functions"):
+            self.code = self.code + function  
+            
+        #Start main function
+        self.code = self.code + "int main(void){"
+        
+        #Add counters
+        for counter in self.counters:
+            self.code = self.code + "int counter_" + counter[0] + " = 0;"
+            self.code = self.code + "int count_" + counter[0] + "_to = " + counter[1] + ";"
+            self.code = self.code + "bool counter_" + counter[0] + "_target = " + counter[2] + ";"
+            
+        self.code = self.code + "\n"
+        self.code = self.code + "\nwhile(1){"
         # TODO add gpio stuff
     
     def appendCondtion(self, condition, output):
         """Add next if condition."""
         
-        self.code = self.code + "if(" + condition + ") {" + output + "; }" 
-        return
+        if output.startswith("C"):
+            outputCode = "int *a = &counter_" + output + ";(*a) ++"
+        else:
+            outputCode = output
+        
+        self.code = self.code + "if(" + condition + ") {" + outputCode + "; }" 
         
     def getCode(self):
         """Return generated code."""
